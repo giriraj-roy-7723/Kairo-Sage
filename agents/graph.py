811cf13a07cf051bd41c44eed71d21c4f35cdb2f
giriraj-roy-7723 <roy.2004002@gmail.com>
2025-09-06 20:43:47 +0530

    architect agent done

--- agents/graph.py
+++ agents/graph.py
@@ -1,5 +1,9 @@
 from dotenv import load_dotenv
 load_dotenv()
+
+from langchain.globals import set_debug,set_verbose
+set_debug(True)
+set_verbose(True)
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 from langchain_groq import ChatGroq
 from pydantic import BaseModel
@@ -18,11 +22,26 @@ from states import *
 from langgraph.constants import END
 from langgraph.graph import StateGraph
 graph=StateGraph(dict)
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#Defining hte planner agent
 def planner_agent(state:dict)->dict:
     user_prompt=state["user_prompt"]
     resp=llm.with_structured_output(Plan).invoke(planner_prompt(user_prompt))
     return ({"plan":resp})
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#Defining the architect agent
+def architect_agent(state:dict)->str:
+    plan:Plan=state["plan"]
+    resp=llm.with_structured_output(TaskPlan).invoke(architect_prompt(plan))
+    if resp is None:
+        raise ValueError("The planner didnot return a valid response")
+    resp.plan=plan
+    return {"task_plan":resp}
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#Defining a coder agent
 graph.add_node("planner",planner_agent)
+graph.add_node("architect",architect_agent)
+graph.add_edge("planner","architect")
 graph.set_entry_point("planner")
 agent=graph.compile()
 resp=agent.invoke({"user_prompt":user_prompt})
--- agents/states.py
+++ agents/states.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel , Field
+from pydantic import BaseModel , Field ,ConfigDict
 
 class File(BaseModel):
     path:str=Field(description="the path to the file to be created or modified")
@@ -9,3 +9,10 @@ class Plan(BaseModel):
     techstack:str=Field(description="The tech stack to be used for the app , e.g. 'python','javascript','react','flask'")
     features:list[str]=Field(description="List of features the app should have ,e.g.'user suthentication','data analysis'")
     files:list[File]=Field(description="A list of files to be created each with a 'path' and a 'purpose'")
+
+class ImplementationTask(BaseModel):
+    filepath:str=Field(description="The path to the file to be modified")
+    task_description:str=Field(description="A detailed description of the task to be performed on the file, e.g. 'add user authentication', 'implement data processing logic', etc.")
+class TaskPlan(BaseModel):
+    implementation_Steps:list[ImplementationTask]=Field(description="The plan for the task to be implemented")
+    model_config = ConfigDict(extra="allow")
